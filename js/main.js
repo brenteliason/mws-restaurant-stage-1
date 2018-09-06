a7be57432bfb300031af14d4d8b4a85870678e0c
Brent <brenteliason@gmail.com>
2018-09-06 10:56:31 -0400

    Registered service worker

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ let restaurants,
 var newMap
 var markers = []
 
+//Establish service worker for caching visited pages for offline use
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('sw.js')
+  .catch(function(err) {
+    console.error(err);
+  });
+}
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
