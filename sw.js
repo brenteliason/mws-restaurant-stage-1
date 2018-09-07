cc56aef81450167851cffc7a655754d5402caad3
Brent <brenteliason@gmail.com>
2018-09-07 07:06:37 -0400

    Updated serviceworker to pull from cache when available or to add to cache when not available

--- sw.js
+++ sw.js
@@ -10,6 +10,34 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('fetch', function(event) {
+  console.log("Fetch event");
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        console.log("Match made with: " + event.request + " in cache");
+        return response;
+      }
+      else {
+        console.log("No match with: " + event.request + " in cache");
+        return fetch(event.request)
+        .then(function(response) {
+          const clonedResponse = response.clone();
+          caches.open('v1').then(function(cache) {
+            cache.put(event.request, clonedResponse);
+          })
+          return response;
+        })
+        .catch(function(err) {
+          console.error(err);
+        })
+      }
+
+
+    })
+  );
+});
+
 console.log("Creating list of files to cache");
 const cacheList = [
   '/index.html',
