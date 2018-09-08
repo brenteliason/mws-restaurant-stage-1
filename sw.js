aff71306195a2ba73c789f1ef7ef4d9db5ef8a61
Brent <brenteliason@gmail.com>
2018-09-08 15:36:07 -0400

    Added comments to SW

--- sw.js
+++ sw.js
@@ -1,30 +1,30 @@
 //console.log('Service Worker: Registered');
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function(event) {//when SW is installed, establish cache
   //console.log("Install event");
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open('restaurant-reviews-v1').then(function(cache) {
       //console.log("Return new cache");
       return cache.addAll(cacheList);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {//hijacks fetch requests
   //console.log("Fetch event");
   event.respondWith(
-    caches.match(event.request).then(function (response) {
+    caches.match(event.request).then(function (response) {//checks for match in cache
       if (response) {
         //console.log("Match made with: " + event.request + " in cache");
-        return response;
+        return response;//returns match if found
       }
-      else {
+      else {//if no match is found, add new file to cache, and then respond to request
         //console.log("No match with: " + event.request + " in cache");
         return fetch(event.request)
         .then(function(response) {
-          const clonedResponse = response.clone();
-          caches.open('v1').then(function(cache) {
-            cache.put(event.request, clonedResponse);
+          const cacheResponse = response.clone();//copy response to add to cache
+          caches.open('restaurant-reviews-v1').then(function(cache) {
+            cache.put(event.request, cacheResponse);
           })
           return response;
         })
