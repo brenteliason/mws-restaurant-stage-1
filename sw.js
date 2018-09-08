69dfb9ad39ac35a818a167efc2d5769fa49a6b4a
Brent <brenteliason@gmail.com>
2018-09-08 15:26:51 -0400

    Commented out console-logs, simplified cacheList

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
-console.log('Service Worker: Registered');
+//console.log('Service Worker: Registered');
 
 self.addEventListener('install', function(event) {
-  console.log("Install event");
+  //console.log("Install event");
   event.waitUntil(
     caches.open('v1').then(function(cache) {
-      console.log("Return new cache");
+      //console.log("Return new cache");
       return cache.addAll(cacheList);
     })
   );
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log("Fetch event");
+  //console.log("Fetch event");
   event.respondWith(
     caches.match(event.request).then(function (response) {
       if (response) {
-        console.log("Match made with: " + event.request + " in cache");
+        //console.log("Match made with: " + event.request + " in cache");
         return response;
       }
       else {
-        console.log("No match with: " + event.request + " in cache");
+        //console.log("No match with: " + event.request + " in cache");
         return fetch(event.request)
         .then(function(response) {
           const clonedResponse = response.clone();
@@ -38,21 +38,11 @@ self.addEventListener('fetch', function(event) {
   );
 });
 
-console.log("Creating list of files to cache");
+//console.log("Creating list of files to cache");
 const cacheList = [
   '/index.html',
   '/restaurant.html',
   '/css/styles.css',
-  '/js/main.js',
-  '/js/restaurant_info.js',
-  '/img/1.jpg',
-  '/img/2.jpg',
-  '/img/3.jpg',
-  '/img/4.jpg',
-  '/img/5.jpg',
-  '/img/6.jpg',
-  '/img/7.jpg',
-  '/img/8.jpg',
-  '/img/9.jpg',
-  '/img/10.jpg',
+  '/js/*',
+  '/img/*',
 ]
