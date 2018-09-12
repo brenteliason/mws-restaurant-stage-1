147eb2dd31276e736b5a18bcf239d7824992dfa2
Brent <brenteliason@gmail.com>
2018-09-12 12:05:03 -0400

    Additional changes based on project review, updated javascript with alt text, changed h1 to h3

--- js/main.js
+++ js/main.js
@@ -173,7 +173,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = "picture of restaurant";
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
