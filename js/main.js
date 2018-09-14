3e485160974d4781085ac2424c00b6db1864d916
Brent <brenteliason@gmail.com>
2018-09-14 10:32:00 -0400

    Made more changes based on second code review, updated aria roles for maps, changed alt text for restaurant pictures

--- js/main.js
+++ js/main.js
@@ -170,7 +170,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = "picture of restaurant";
+  image.alt = "picture of " + restaurant.name + " restaurant";
   li.append(image);
 
   const name = document.createElement('h3');
